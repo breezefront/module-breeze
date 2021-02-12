2e8a4d4fb3de19e57dd65e193b798eae82fd8ca8
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-02-12 17:51:01 +0200

    Default values for js cookies

--- view/frontend/web/js/components/cookies.js
+++ view/frontend/web/js/components/cookies.js
@@ -3,6 +3,13 @@
     'use strict';
 
     $(document).on('breeze:mount:mage/cookies', function (event) {
-        breeze.cookies = Cookies.withAttributes(event.detail.settings);
+        breeze.cookies = Cookies.withAttributes($.extend({
+            path: '/',
+            domain: null,
+            secure: false,
+            expires: null,
+            lifetime: null,
+            samesite: 'strict'
+        }, event.detail.settings));
     });
 })();
