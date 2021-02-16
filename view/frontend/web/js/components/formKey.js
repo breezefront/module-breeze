238e3b07cc0f679e1afdb7c8bfb6507e7a34d156
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-02-16 17:44:41 +0200

    Update form key when page is cached by fpc

---
+++ view/frontend/web/js/components/formKey.js
@@ -0,0 +1,39 @@
+/* global breeze */
+(function () {
+    'use strict';
+
+    /**
+     * Generate form key string
+     * @private
+     */
+    function generateFormKeyString() {
+        var result = '',
+            length = 16,
+            chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        while (length--) {
+            result += chars[Math.round(Math.random() * (chars.length - 1))];
+        }
+
+        return result;
+    }
+
+    /**
+     * Init form_key inputs with value
+     * @private
+     */
+    function initFormKey() {
+        var formKey = breeze.cookies.get('form_key');
+
+        if (!formKey) {
+            formKey = generateFormKeyString();
+            breeze.cookies.set('form_key', formKey);
+        }
+
+        $('input[name="form_key"]').val(formKey);
+    }
+
+    document.addEventListener('breeze:mount:Magento_PageCache/js/form-key-provider', function () {
+        initFormKey();
+    });
+})();
