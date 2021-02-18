729cca4d590c9e395b7b4a76f04234e0edbc583c
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-02-18 17:02:50 +0200

    Compare products view model added

---
+++ view/frontend/web/js/components/view/compareProducts.js
@@ -0,0 +1,12 @@
+/* global breeze */
+(function () {
+    'use strict';
+
+    breeze.view('compareProducts', {
+        compareProducts: breeze.sections.get('compare-products')
+    });
+
+    document.addEventListener('breeze:mount:Magento_Catalog/js/view/compare-products', function (event) {
+        $(event.detail.el).compareProducts(event.detail.settings);
+    });
+})();
