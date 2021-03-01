bb334965f70cfc44237b63ae8f5d112e9de9029c
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-03-01 11:23:13 +0200

    Add to cart at product page

---
+++ view/frontend/web/js/components/validateProduct.js
@@ -0,0 +1,15 @@
+/* global breeze */
+(function () {
+    'use strict';
+
+    breeze.widget('productValidate', {
+        /** Initialize plugin */
+        init: function () {
+            $(this.element).catalogAddToCart(this.options);
+        }
+    });
+
+    document.addEventListener('breeze:mount:Magento_Catalog/js/validate-product', function (event) {
+        $(event.detail.el).productValidate(event.detail.settings);
+    });
+})();
