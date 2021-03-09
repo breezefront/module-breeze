b436f9740196c12203f61d601726d1752a89d541
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-03-09 17:49:08 +0200

    Paypal buttons support added

---
+++ view/frontend/web/js/components/paypalCheckout.js
@@ -0,0 +1,65 @@
+/* global breeze */
+(function () {
+    'use strict';
+
+    breeze.widget('paypalCheckout', {
+        options: {
+            productId: 'input[type="hidden"][name="product"]',
+            ppCheckoutSelector: '[data-role=pp-checkout-url]',
+            ppCheckoutInput: '<input type="hidden" data-role="pp-checkout-url" name="return_url" value=""/>'
+        },
+
+        /** Init widget */
+        init: function () {
+            this.agreements = breeze.sections.get('paypal-billing-agreement');
+            this.onClickHandler = this.onClick.bind(this);
+            $(this.element).on('click', '[data-action="checkout-form-submit"]', this.onClickHandler);
+        },
+
+        /** Destroy widget listeners */
+        destroy: function () {
+            $(this.element).off('click', this.onClickHandler);
+        },
+
+        /** [onClick description] */
+        onClick: function (event) {
+            var target = $(event.target),
+                returnUrl = target.data('checkout-url'),
+                form = target.closest('form'),
+                productId = form.find(this.options.productId).val(),
+                originalForm = productId ? form : false;
+
+            event.preventDefault();
+
+            if (this.agreements().askToCreate && confirm(this.agreements().confirmMessage)) { // eslint-disable-line
+                this.redirect(this.agreements().confirmUrl, originalForm);
+            } else {
+                this.redirect(returnUrl, originalForm);
+            }
+        },
+
+        /** Initialize plugin */
+        redirect: function (returnUrl, form) {
+            var ppCheckoutInput;
+
+            if (form) {
+                ppCheckoutInput = form.find(this.options.ppCheckoutSelector)[0];
+
+                if (!ppCheckoutInput) {
+                    ppCheckoutInput = $(this.options.ppCheckoutInput);
+                    ppCheckoutInput.appendTo(form);
+                }
+
+                $(ppCheckoutInput).val(returnUrl);
+
+                form.submit();
+            } else {
+                window.location.assign(returnUrl);
+            }
+        }
+    });
+
+    $(document).on('breeze:mount:paypalCheckout', function (event) {
+        $(event.detail.el).paypalCheckout(event.detail.settings);
+    });
+})();
