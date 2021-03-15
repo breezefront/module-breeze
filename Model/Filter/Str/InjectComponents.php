a984956e49e5c2ffb6cace1fad56fbfe468d67c3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-03-15 09:11:00 +0200

    Remove unused code (body already has a plenty of script tags)

--- Model/Filter/Str/InjectComponents.php
+++ Model/Filter/Str/InjectComponents.php
@@ -12,9 +12,6 @@ class InjectComponents extends AbstractFilter
      */
     public function process($html)
     {
-        // fix jump for a few pixel when refreshing the page.
-        $html = str_replace('</body>', '<script> </script></body>', $html);
-
         return str_replace(
             '</head>',
             sprintf("\n%s\n</head>", $this->getJsBlock()->toHtml()),
