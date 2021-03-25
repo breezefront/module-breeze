1231f2cd4a385d98c546b85eeb1e9cf505f77d33
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-03-25 14:47:58 +0200

    CustomerAssistance support added

---
+++ view/frontend/web/js/components/customerAssistance.js
@@ -0,0 +1,23 @@
+/* global breeze */
+(function () {
+    'use strict';
+
+    breeze.widget('customerAssistance', {
+        /** Initialize plugin */
+        create: function () {
+            var self = this;
+
+            //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+            $(this.element).on('submit', function () {
+                this.elements.assistance_allowed.value =
+                    this.elements.assistance_allowed_checkbox.checked ?
+                        self.options.allowAccess : self.options.denyAccess;
+            });
+            //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+        }
+    });
+
+    $(document).on('breeze:mount:Magento_LoginAsCustomerAssistance/js/opt-in', function (event) {
+        $(event.detail.el).customerAssistance(event.detail.settings);
+    });
+})();
