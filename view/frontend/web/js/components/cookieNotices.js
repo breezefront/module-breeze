f4f735e12467d580613105f0cd289dde48692620
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-03-25 17:53:55 +0200

    CookieRestriction mode

---
+++ view/frontend/web/js/components/cookieNotices.js
@@ -0,0 +1,36 @@
+/* global breeze */
+(function () {
+    'use strict';
+
+    breeze.widget('cookieNotices', {
+        /** Initialize plugin */
+        create: function () {
+            var self = this;
+
+            if (breeze.cookies.get(this.options.cookieName)) {
+                $(this.element).hide();
+            } else {
+                $(this.element).show();
+            }
+
+            $(this.options.cookieAllowButtonSelector).on('click', function () {
+                var cookieExpires = new Date(new Date().getTime() + self.options.cookieLifetime * 1000);
+
+                breeze.cookies.set(self.options.cookieName, JSON.stringify(self.options.cookieValue), {
+                    expires: cookieExpires
+                });
+
+                if (breeze.cookies.get(self.options.cookieName)) {
+                    $(self.element).hide();
+                    $(document).trigger('user:allowed:save:cookie');
+                } else {
+                    window.location.href = self.options.noCookiesUrl;
+                }
+            });
+        }
+    });
+
+    document.addEventListener('breeze:mount:cookieNotices', function (event) {
+        $(event.detail.el).cookieNotices(event.detail.settings);
+    });
+})();
