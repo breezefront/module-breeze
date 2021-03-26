0c20a885c921749f3a1320168609f297af492543
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-03-26 10:29:49 +0200

    Persistent shopping cart component

---
+++ view/frontend/web/js/components/persistent.js
@@ -0,0 +1,41 @@
+/* global breeze customerData $t */
+(function () {
+    'use strict';
+
+    breeze.widget('persistent', {
+        /** Initialize plugin */
+        create: function () {
+            var persistent = customerData.get('persistent');
+
+            if (persistent().fullname === undefined) {
+                customerData.get('persistent').subscribe(this.replacePersistentWelcome);
+            } else {
+                this.replacePersistentWelcome();
+            }
+        },
+
+        /** Replace welcome message for customer with persistent cookie. */
+        replacePersistentWelcome: function () {
+            var persistent = customerData.get('persistent'),
+                welcomeElems;
+
+            if (persistent().fullname !== undefined) {
+                welcomeElems = $('li.greet.welcome > span.not-logged-in');
+
+                if (welcomeElems.length) {
+                    $(welcomeElems).each(function () {
+                        var html = $t('Welcome, %1!').replace('%1', persistent().fullname);
+
+                        $(this).attr('data-bind', html);
+                        $(this).html(html);
+                    });
+                    $(welcomeElems).append(' <span><a ' + window.notYouLink + '>' + $t('Not you?') + '</a>');
+                }
+            }
+        }
+    });
+
+    document.addEventListener('breeze:mount:Magento_Persistent/js/view/additional-welcome', function (event) {
+        $(event.detail.el).persistent(event.detail.settings);
+    });
+})();
