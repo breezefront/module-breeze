fcaad04de1c0223fe6cc77136e1a3d793fa1b531
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-04-13 09:58:59 +0300

    Do not add breeze scripts to the bundle

---
+++ Plugin/BundleConfig.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Swissup\Breeze\Plugin;
+
+class BundleConfig
+{
+    /**
+     * @param \Magento\Deploy\Config\BundleConfig $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterGetExcludedDirectories(
+        $subject,
+        $result
+    ) {
+        $result[] = 'Swissup_Breeze::js'; // module sources
+        $result[] = 'Lib::js/breeze/';    // theme and module integrations
+
+        return $result;
+    }
+
+    /**
+     * @param \Magento\Deploy\Config\BundleConfig $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterGetExcludedFiles(
+        $subject,
+        $result
+    ) {
+        $result[] = 'Lib::js/breeze.js'; // theme and module integrations
+
+        return $result;
+    }
+}
