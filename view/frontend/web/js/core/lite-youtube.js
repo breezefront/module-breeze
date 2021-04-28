b4cd6eb2648cd24b40eff89f2cbf0952e6198373
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-04-28 09:31:27 +0300

    Inline youtube-lite css to reduce load time

--- view/frontend/web/js/core/lite-youtube.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    var link = $('<link type="text/css" rel="stylesheet">');
-
-    link.attr('href', require.baseUrl + '/js/lib/lite-yt-embed.css');
-
-    $('head').append(link);
-})();
