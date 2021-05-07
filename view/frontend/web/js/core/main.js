e7f430c23d875191b94c95a7187e3de751ee0205
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-05-07 17:25:39 +0300

    Do not remove x-magento-init as they may be used for ajax loaded content

--- view/frontend/web/js/core/main.js
+++ view/frontend/web/js/core/main.js
@@ -54,50 +54,39 @@
 
         $.each(settings, function (component, config) {
             if (isScript) {
-                component = Object.keys(config)[0];
-                config = Object.values(config)[0];
-            }
+                el = false;
 
-            if (component === 'Magento_Ui/js/core/app') {
-                $.each(config.components, function (scope, cfg) {
-                    cfg.__scope = scope;
-                    mountView(scope, cfg);
-                });
+                if (component !== '*') {
+                    el = $(component);
+
+                    // eslint-disable-next-line max-depth
+                    if (!el.length) {
+                        return;
+                    }
+                }
+
+                component = Object.keys(config);
+                config = Object.values(config);
             } else {
-                mount(component, {
-                    settings: config,
-                    el: el
-                });
-            }
-        });
-    }
-
-    /** Create data-mage-init attributes for all matches elements based on script contents */
-    function convertScriptsToDataMageInit(script) {
-        var settings;
-
-        try {
-            settings = JSON.parse(script.textContent);
-        } catch (e) {
-            console.error(e);
-        }
-
-        $.each(settings, function (selector, components) {
-            if (selector === '*') {
-                return;
+                component = [component];
+                config = [config];
             }
 
-            $('html').find(selector).each(function () {
-                $(this).attr(
-                    'data-mage-init',
-                    JSON.stringify($.extend($(this).data('mage-init') || {}, components))
-                );
+            $.each(component, function (i, name) {
+                if (name === 'Magento_Ui/js/core/app') {
+                    // eslint-disable-next-line max-nested-callbacks
+                    $.each(config[i].components, function (scope, cfg) {
+                        cfg.__scope = scope;
+                        mountView(scope, cfg);
+                    });
+                } else {
+                    mount(name, {
+                        settings: config[i],
+                        el: el
+                    });
+                }
             });
-
-            delete settings[selector];
         });
-
-        script.textContent = JSON.stringify(settings);
     }
 
     /** Extract json by key from dataBind string */
@@ -161,9 +150,6 @@
      */
     function walk(node) {
         node = node || document;
-
-        node.querySelectorAll('[type="text/x-magento-init"]')
-            .forEach(convertScriptsToDataMageInit);
 
         node.querySelectorAll('[data-bind*="mageInit:"]')
             .forEach(convertDataBindToDataMageInit);
