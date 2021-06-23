a98dcdd9652f492c13452bf4b70f39b66e8125ee
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-06-23 14:28:02 +0300

    Fixed ability to extend widget options with nested arrays and functions

--- view/frontend/web/js/lib/class.js
+++ view/frontend/web/js/lib/class.js
@@ -3,9 +3,46 @@
  * Inspired by base2 and Prototype
  * MIT Licensed.
  */
+/* global _ */
+// eslint-disable-next-line strict
 (function () {
     var initializing = false,
+        // eslint-disable-next-line no-undef
         fnTest = /xyz/.test(function () {xyz;}) ? /\b_super\b/ : /.*/;
+
+    /**
+     * Create new object with extended props.
+     * Objects are extended, arrays are overwritten.
+     *
+     * @param {Object} parent
+     * @param {Object} self
+     * @return {Object}
+     */
+    function recursiveExtend(parent, self) {
+        var destination = {};
+
+        $.each(self, function (key, value) {
+            if (_.isObject(value) && !_.isArray(value) && !_.isFunction(value)) {
+                destination[key] = recursiveExtend(parent[key] || {}, self[key]);
+            } else {
+                destination[key] = value;
+            }
+        });
+
+        $.each(parent, function (key, value) {
+            if (typeof self[key] !== 'undefined') {
+                return;
+            }
+
+            if (_.isObject(value) && !_.isArray(value) && !_.isFunction(value)) {
+                destination[key] = recursiveExtend(parent[key], self[key] || {});
+            } else {
+                destination[key] = value;
+            }
+        });
+
+        return destination;
+    }
 
     this.Class = function () {};
 
@@ -20,19 +57,20 @@
         prototype = new this();
         initializing = false;
 
+        // eslint-disable-next-line guard-for-in
         for (name in prop) {
             propType = typeof prop[name];
             superPropType = typeof _super[name];
 
             if (propType === 'function' && superPropType === 'function' && fnTest.test(prop[name])) {
-                prototype[name] = (function (name, fn) {
+                prototype[name] = (function (key, fn) {
                     return function () {
                         var tmp = this._super,
                             ret;
 
                         // Add a new ._super() method that is the same method
                         // but on the super-class
-                        this._super = _super[name];
+                        this._super = _super[key];
 
                         // The method only need to be bound temporarily, so we
                         // remove it when we're done executing
@@ -44,7 +82,7 @@
                     };
                 })(name, prop[name]);
             } else if (propType === 'object' && superPropType === 'object') {
-                prototype[name] = $.extend(true, {}, _super[name], prop[name]);
+                prototype[name] = recursiveExtend(_super[name], prop[name]);
             } else {
                 prototype[name] = prop[name];
             }
@@ -59,6 +97,7 @@
 
         Class.prototype = prototype;
         Class.prototype.constructor = Class;
+        // eslint-disable-next-line no-caller
         Class.extend = arguments.callee;
 
         return Class;
