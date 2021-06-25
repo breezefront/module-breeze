f2d4bd0efe77a34df149739bcd084f5db8cebe1f
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-06-25 14:43:10 +0300

    Wrap all methods into anonymous function

--- view/frontend/web/js/core/extend-ko.js
+++ view/frontend/web/js/core/extend-ko.js
@@ -1,50 +1,48 @@
-/* global ko */
-ko.bindingHandlers.blockLoader = {
-    /**
-     * Process loader for block
-     * @param {String} element
-     * @param {Boolean} displayBlockLoader
-     */
-    update: function (element, displayBlockLoader) {
-        'use strict';
+/* global ko _ */
+(function () {
+    'use strict';
 
-        if (ko.unwrap(displayBlockLoader())) {
-            $(element).spinner(true);
-        } else {
-            $(element).spinner(false);
+    ko.bindingHandlers.blockLoader = {
+        /**
+         * Process loader for block
+         * @param {String} element
+         * @param {Boolean} displayBlockLoader
+         */
+        update: function (element, displayBlockLoader) {
+            if (ko.unwrap(displayBlockLoader())) {
+                $(element).spinner(true);
+            } else {
+                $(element).spinner(false);
+            }
         }
-    }
-};
+    };
 
-ko.bindingHandlers.i18n = {
-    /**
-     * @param {Object} element
-     * @param {Function} value
-     */
-    update: function (element, value) {
-        'use strict';
+    ko.bindingHandlers.i18n = {
+        /**
+         * @param {Object} element
+         * @param {Function} value
+         */
+        update: function (element, value) {
+            $(element).text($.__(ko.unwrap(value() || '')));
+        }
+    };
 
-        $(element).text($.__(ko.unwrap(value() || '')));
-    }
-};
+    ko.bindingHandlers.mageInit = {
+        /**
+         * Initializes components assigned to HTML elements.
+         *
+         * @param {HTMLElement} el
+         * @param {Function} valueAccessor
+         */
+        init: function (el, valueAccessor) {
+            var data = valueAccessor();
 
-ko.bindingHandlers.mageInit = {
-    /**
-     * Initializes components assigned to HTML elements.
-     *
-     * @param {HTMLElement} el
-     * @param {Function} valueAccessor
-     */
-    init: function (el, valueAccessor) {
-        'use strict';
-
-        var data = valueAccessor();
-
-        _.each(data, function (config, component) {
-            window.breeze.mount(component, {
-                settings: config,
-                el: el
+            _.each(data, function (config, component) {
+                window.breeze.mount(component, {
+                    settings: config,
+                    el: el
+                });
             });
-        });
-    }
-};
+        }
+    };
+})();
