ddbb267538c00747b52c666271b2cf3e4abfcf59
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-07-15 10:00:20 +0300

    Prevent CLS at product page when critical CSS is enabled. Closes #15

---
+++ Plugin/CriticalCss.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Swissup\Breeze\Plugin;
+
+class CriticalCss
+{
+    /**
+     * @var \Swissup\Breeze\Helper\Data
+     */
+    private $helper;
+
+    /**
+     * @var \Magento\Framework\View\Asset\Repository
+     */
+    private $assetRepo;
+
+    /**
+     * @param \Swissup\Breeze\Helper\Data $helper
+     * @param \Magento\Framework\View\Asset\Repository $assetRepo
+     */
+    public function __construct(
+        \Swissup\Breeze\Helper\Data $helper,
+        \Magento\Framework\View\Asset\Repository $assetRepo
+    ) {
+        $this->helper = $helper;
+        $this->assetRepo = $assetRepo;
+    }
+
+    /**
+     * @param \Magento\Theme\Block\Html\Header\CriticaCss $subject
+     * @param string $result
+     * @return string
+     */
+    public function afterGetCriticalCssData(
+        $subject,
+        $result
+    ) {
+        if (empty($result)) {
+            return $result;
+        }
+
+        try {
+            $content = $this->assetRepo
+                ->createAsset('Swissup_Breeze::css/critical.css', [
+                    '_secure' => 'false'
+                ])
+                ->getContent();
+
+            $content = str_replace("\n", ' ', $content);
+            $content = preg_replace('/\s\s+/', ' ', $content);
+            $result = $content . $result;
+        } catch (\Exception $e) {
+            //
+        }
+
+        return $result;
+    }
+}
