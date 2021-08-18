4002eebe4269f569ad264038de1a17dd90feac6e
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-08-18 10:08:05 +0300

    Magento 2.4.3: Fixed disabled 'Save Address' button in customer account.

--- view/frontend/web/js/components/region-updater.js
+++ view/frontend/web/js/components/region-updater.js
@@ -218,6 +218,7 @@
 
             // Add defaultvalue attribute to state/province select element
             regionList.attr('defaultvalue', this.options.defaultRegion);
+            this.options.form.find('[type="submit"]').removeAttr('disabled').show();
         },
 
         /**
