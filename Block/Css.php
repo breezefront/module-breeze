698dfdfcd8b432ea3f95de9ca465b485b286a262
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-11-18 17:31:37 +0200

    Allow to pass params to included css files

--- Block/Css.php
+++ Block/Css.php
@@ -12,8 +12,10 @@ class Css extends \Magento\Framework\View\Element\AbstractBlock
         $items = [];
 
         foreach ($this->getBundles() ?? [] as $name => $data) {
-            $items[] = $this->renderLink($name);
-            $items[] = $this->renderLink($name, true);
+            $items[] = $this->renderLink($name, $data);
+            if (!isset($data['deferred']) || $data['deferred']) {
+                $items[] = $this->renderLink($name, $data, true);
+            }
         }
 
         return implode("\n", $items);
@@ -21,16 +23,23 @@ class Css extends \Magento\Framework\View\Element\AbstractBlock
 
     /**
      * @param string $name
+     * @param array $data
      * @param boolean $deferred
      * @return string
      */
-    private function renderLink($name, $deferred = false)
+    private function renderLink($name, $data = [], $deferred = false)
     {
+        $media = 'all';
+
+        if (!empty($data['media'])) {
+            $media = $data['media'];
+        }
+
         if ($deferred) {
-            $template = '<link rel="stylesheet" type="text/css" media="print" onload="media=\'all\'" href="%s"/>';
+            $template = '<link rel="stylesheet" type="text/css" media="print" onload="media=\'' . $media . '\'" href="%s"/>';
             $href = 'css/deferred-' . $name . '.css';
         } else {
-            $template = '<link rel="stylesheet" type="text/css" media="all" href="%s"/>';
+            $template = '<link rel="stylesheet" type="text/css" media="' . $media . '" href="%s"/>';
             $href = 'css/' . $name . '.css';
         }
 
