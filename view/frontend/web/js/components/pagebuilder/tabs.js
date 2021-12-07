ec54a3c56bcce098f40c388a9ce7777af604b254
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-12-07 14:13:41 +0200

    Pagebuilder tabs support

---
+++ view/frontend/web/js/components/pagebuilder/tabs.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    $.widget('pagebuilderTabs', {
+        component: 'Magento_PageBuilder/js/content-type/tabs/appearance/default/widget',
+
+        /** [create description] */
+        create: function () {
+            this.element.tabs({
+                collapsibleElement: '[role=tab]',
+                header: '[data-role=tab]',
+                content: '[data-content-type="tab-item"]',
+                trigger: '[data-role=tab]'
+            });
+        }
+    });
+})();
