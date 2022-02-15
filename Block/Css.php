baf755b587fe44f176251a9dbf7194774d9c6db4
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-02-15 10:42:31 +0200

    Added support for Magento's "Use CSS critical path" config option

--- Block/Css.php
+++ Block/Css.php
@@ -4,21 +4,46 @@ namespace Swissup\Breeze\Block;
 
 class Css extends \Magento\Framework\View\Element\AbstractBlock
 {
+    private $assetRepo;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Context $context,
+        \Magento\Framework\View\Asset\Repository $assetRepo,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+
+        $this->assetRepo = $assetRepo;
+    }
+
     /**
      * @return string
      */
     protected function _toHtml()
     {
         $items = [];
+        $inlineCss = $this->_scopeConfig->getValue(
+            'dev/css/use_css_critical_path',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
 
         foreach ($this->getBundles() ?? [] as $name => $data) {
-            $items[] = $this->renderLink($name, $data);
+            if ($inlineCss) {
+                $items[] = $this->renderCss($name);
+            } else {
+                $items[] = $this->renderLink($name, $data);
+            }
             if (!isset($data['deferred']) || $data['deferred']) {
-                $items[] = $this->renderLink($name, $data, true);
+                if ($inlineCss) {
+                    // magento will defer this style on it's own when inlineCss is used
+                    $items[] = $this->renderLink('deferred-' . $name, $data);
+                } else {
+                    $items[] = $this->renderLink($name, $data, true);
+                }
             }
         }
 
-        return implode("\n", $items);
+        return implode("\n", array_filter($items));
     }
 
     /**
@@ -44,5 +69,24 @@ class Css extends \Magento\Framework\View\Element\AbstractBlock
         }
 
         return sprintf($template, $this->getViewFileUrl($href));
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private function renderCSS($name)
+    {
+        try {
+            $asset = $this->assetRepo->createAsset('css/' . $name . '.css', ['_secure' => 'false']);
+            $content = $asset->getContent();
+        } catch (\Exception $e) {
+            return '';
+        }
+
+        $content = str_replace("\n", ' ', $content);
+        $content = preg_replace('/\s{2,}/', ' ', $content);
+
+        return '<style>' . $content . '</style>';
     }
 }
