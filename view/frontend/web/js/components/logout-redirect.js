1354a9530e1f766ae8400d3115ee9f8c3d6ed562
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-09-12 11:24:26 +0300

    Remove redundant logic (Sync code with original component)

--- view/frontend/web/js/components/logout-redirect.js
+++ view/frontend/web/js/components/logout-redirect.js
@@ -2,11 +2,8 @@
     'use strict';
 
     $(document).on('breeze:mount:Magento_Customer/js/logout-redirect', function (event, data) {
-        $.sections.reload($.sections.getSectionNames());
-        $(document).on('customer-data-reload', function () {
-            setTimeout(() => {
-                window.location.href = data.settings.url;
-            }, 5000);
-        });
+        setTimeout(() => {
+            window.location.href = data.settings.url;
+        }, 5000);
     });
 })();
