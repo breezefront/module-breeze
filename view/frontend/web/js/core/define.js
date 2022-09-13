5fdf4efeb77cb61ad49b2c23b694bc8e3f8bac5e
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-09-13 17:54:43 +0300

    Improve ability to reuse Luma-based files. Custom aliases to use in `define`

--- view/frontend/web/js/core/define.js
+++ view/frontend/web/js/core/define.js
@@ -1,27 +1,32 @@
 (function () {
+    $.defineMap = [
+        'jquery': $,
+        'ko': ko,
+        'mage/cookies': $.cookies,
+        'mage/translate': $.__,
+        'Magento_Customer/js/customer-data': $.sections,
+        'Magento_Ui/js/lib/view/utils/async': $,
+        'underscore': _
+    ];
+
+    function resolve(alias) {
+        // @todo: text!, functions, urls
+        return $.defineMap[alias] || undefined;
+    }
+
     /**
      * @param {Array} deps
      * @param {Function} callback
      */
     window.define = function (deps, callback) {
-        var args = [],
-            mapping = {
-                'jquery': $,
-                'ko': ko,
-                'mage/cookies': $.cookies,
-                'mage/translate': $.__,
-                'Magento_Customer/js/customer-data': $.sections,
-                'Magento_Ui/js/lib/view/utils/async': $,
-                'underscore': _
-            };
+        var args = [];
 
         if (!_.isArray(deps)) {
             return;
         }
 
         deps.forEach(function (alias) {
-            // @todo: text!, functions, urls
-            args.push(mapping[alias] || undefined);
+            args.push(resolve(alias));
         });
 
         callback.apply(this, args);
