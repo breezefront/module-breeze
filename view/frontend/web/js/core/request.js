d9d3668db2934da57cb9902685d20e8383086615
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-03-29 12:32:24 +0300

    Remove unused code

--- view/frontend/web/js/core/request.js
+++ view/frontend/web/js/core/request.js
@@ -160,25 +160,6 @@
         return formData;
     }
 
-    function toJsonData(formData) {
-        var object = {};
-
-        formData.forEach((value, key) => {
-            if (!Reflect.has(object, key)) {
-                object[key] = value;
-                return;
-            }
-
-            if (!Array.isArray(object[key])) {
-                object[key] = [object[key]];
-            }
-
-            object[key].push(value);
-        });
-
-        return object;
-    }
-
     function send(params) {
         if (params.method === 'get' && params.data) {
             params.url += params.url.indexOf('?') === -1 ? '?' : '&';
