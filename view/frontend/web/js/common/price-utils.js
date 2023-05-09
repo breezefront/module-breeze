a3bb2835746dab822e90858a93919e987927ae72
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-05-09 17:56:21 +0300

    Copied new formatPriceLocale function from Magento sources.

--- view/frontend/web/js/common/price-utils.js
+++ view/frontend/web/js/common/price-utils.js
@@ -20,6 +20,34 @@
     }
 
     /**
+     * Format the price with the compliance to the specified locale
+     *
+     * @param {Number} amount
+     * @param {Object} format
+     * @param  {Boolean} isShowSign
+     */
+    function formatPriceLocale(amount, format, isShowSign)
+    {
+        var s = '',
+            precision, pattern, locale, r;
+
+        format = _.extend(globalPriceFormat, format);
+        precision = isNaN(format.requiredPrecision = Math.abs(format.requiredPrecision)) ? 2 : format.requiredPrecision;
+        pattern = format.pattern || '%s';
+        locale = window.LOCALE || 'en-US';
+        if (isShowSign === undefined || isShowSign === true) {
+            s = amount < 0 ? '-' : isShowSign ? '+' : '';
+        } else if (isShowSign === false) {
+            s = '';
+        }
+        pattern = pattern.indexOf('{sign}') < 0 ? s + pattern : pattern.replace('{sign}', s);
+        amount = Number(Math.round(Math.abs(+amount || 0) + 'e+' + precision) + ('e-' + precision));
+        r = amount.toLocaleString(locale, {minimumFractionDigits: precision});
+
+        return pattern.replace('%s', r).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    }
+
+    /**
      * Formatter for price amount
      * @param  {Number}  amount
      * @param  {Object}  format
@@ -109,6 +137,7 @@
 
     $.catalog = $.catalog || {};
     $.catalog.priceUtils = {
+        formatPriceLocale: formatPriceLocale,
         formatPrice: formatPrice,
         deepClone: objectDeepClone,
         strPad: stringPad,
