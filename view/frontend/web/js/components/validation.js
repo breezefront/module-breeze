a6976f6ca13a9362e3f42795d838d1dee8443038
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-08-10 15:53:11 +0300

    Allow to get validator instance using `.data('validator')` method

--- view/frontend/web/js/components/validation.js
+++ view/frontend/web/js/components/validation.js
@@ -10,8 +10,8 @@
             this.validator = $.validator(this.element, this.options);
 
             // do not use event delegation because this callback should be first
-            if (!this.element.attr('data-validator')) {
-                this.element.attr('data-validator', true).on('submit', function () {
+            if (!this.element.attr('data-validator-ready')) {
+                this.element.attr('data-validator-ready', true).on('submit', function () {
                     if (!self.validator.isValid()) {
                         return false;
                     }
@@ -20,7 +20,7 @@
         },
 
         destroy: function () {
-            this.element.removeAttr('data-validator');
+            this.element.removeAttr('data-validator-ready');
             this._super();
         },
 
@@ -61,7 +61,7 @@
 
     $(document).on('breeze:load', function () {
         $('form').attr('novalidate', true).on('submit', function () {
-            if ($(this).attr('data-validator')) {
+            if ($(this).attr('data-validator-ready')) {
                 return;
             }
 
