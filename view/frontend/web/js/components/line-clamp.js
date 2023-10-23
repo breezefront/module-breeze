687db974185b011dd8a8875d9cf195143acf7af3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-10-23 14:00:12 +0300

    LineClamp widget added. Use in pair with `.breeze-line-clamp` css.

---
+++ view/frontend/web/js/components/line-clamp.js
@@ -0,0 +1,64 @@
+(() => {
+    'use strict';
+
+    $.widget('lineClamp', {
+        component: 'lineClamp',
+        options: {
+            collapsible: false,
+            textExpand: $t('Show more'),
+            textCollapse: $t('Show less'),
+        },
+
+        create() {
+            this.textExpand = this.element.data('text-expand') || this.options.textExpand;
+            this.textCollapse = this.element.data('text-collapse') || this.options.textCollapse;
+
+            this.prepareMarkup();
+            this.refresh();
+            new ResizeObserver(this.refresh.bind(this)).observe(this.element[0]);
+
+            this._on('click .clamp-toggle', (e) => {
+                e.preventDefault();
+                this.toggle();
+            });
+        },
+
+        prepareMarkup() {
+            this.element.prepend(`<a href="#" class="clamp-toggle" data-breeze-temporary>${this.textExpand}</a>`);
+            this.toggler = this.element.find('.clamp-toggle');
+        },
+
+        refresh() {
+            var isFullyVisible = this.element[0].scrollHeight === this.element[0].clientHeight;
+
+            // hide toggler if content is fully visible without expanding
+            if (!this.element.hasClass('expanded') && isFullyVisible) {
+                return this.toggler.hide();
+            }
+
+            this.toggler.text(isFullyVisible ? this.textCollapse : this.textExpand).show();
+            this.element.toggleClass('expanded', isFullyVisible);
+
+            if (isFullyVisible && !this.options.collapsible) {
+                this.toggler.hide();
+            }
+        },
+
+        toggle() {
+            if (this.element.hasClass('expanded')) {
+                this.collapse();
+            } else {
+                this.expand();
+            }
+        },
+
+        expand() {
+            this.element.addClass('expanded');
+        },
+
+        collapse() {
+            this.element.removeClass('expanded');
+            this.refresh(); // in case if toggler shouldn't be visible
+        }
+    });
+})();
