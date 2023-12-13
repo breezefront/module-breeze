75c99fe45b934abf1515f3567d2abab6ddc39555
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-12-13 16:44:02 +0200

    Do not load script if it's already loaded

--- view/frontend/web/js/core/load-script.js
+++ view/frontend/web/js/core/load-script.js
@@ -5,12 +5,18 @@
         return new Promise((resolve, reject) => {
             var script = document.createElement('script');
 
+            if ($.breeze.loadedScripts[src]) {
+                return resolve();
+            }
+
             script.onload = resolve;
             script.onerror = reject;
             script.src = src;
 
             document.head.appendChild(script);
         }).then(() => {
+            $.breeze.loadedScripts[src] = true;
+
             if (callback) {
                 callback();
             }
