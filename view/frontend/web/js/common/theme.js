30c721269b1b9e32f05a624b7317ddffa6459eff
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-02-01 13:31:38 +0200

    Lazy load backgrounds for pagebuilder elements

--- view/frontend/web/js/common/theme.js
+++ view/frontend/web/js/common/theme.js
@@ -6,5 +6,14 @@
             .clone()
             .data('breeze-temporary', true)
             .appendTo(document.getElementById('store.links'));
+
+        // see Model/Filter/Dom/LazyLoadBackground.php
+        $.async('.breeze-bg-lazy', el => {
+            $.onReveal(el, () => {
+                $(el).css('background-image', '');
+            }, {
+                rootMargin: '100px',
+            });
+        });
     });
 })();
