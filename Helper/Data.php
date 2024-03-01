10291f666dfcbac0399834a7565a3dc055688481
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-03-01 10:43:59 +0200

    SendFriend page support added

--- Helper/Data.php
+++ Helper/Data.php
@@ -85,7 +85,6 @@ class Data extends AbstractHelper
             $excludedUrls[$i] = trim($url);
         }
 
-        $excludedUrls[] = '/sendfriend/product/';
         $excludedUrls[] = '/redirect/';
         // $excludedUrls[] = '/checkout/';
         $excludedUrls[] = '/multishipping/';
