ebffdffdb59c6c65aa7f6c513e1ee3389cdcfc07
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-04-18 11:35:07 +0300

    Add 'js' class to the html element when js is enabled

---
+++ Model/Filter/Str/InjectJsClass.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Swissup\Breeze\Model\Filter\Str;
+
+use Swissup\Breeze\Model\Filter\AbstractFilter;
+
+class InjectJsClass extends AbstractFilter
+{
+    /**
+     * @param  string $html
+     * @return string
+     */
+    public function process($html)
+    {
+        $block = $this->layout->getBlock('breeze.jsClass');
+
+        if (!$block) {
+            return $html;
+        }
+
+        return $this->insertBefore($html, '<script ', $block->toHtml());
+    }
+}
