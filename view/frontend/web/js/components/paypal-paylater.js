8acb5036e376080e130022681f11a78713ea3be9
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-05-02 17:38:24 +0300

    Fix Paypal PayLater inside ajax popup and turbolinks

--- view/frontend/web/js/components/paypal-paylater.js
+++ view/frontend/web/js/components/paypal-paylater.js
@@ -5,7 +5,13 @@
         initialize: function (parent) {
             this.amountComponentConfig.name = `${this.name}.amountProvider`;
             this.amountComponentConfig.parentName = this.name;
+            $.registry.delete(this.amountComponentConfig.name, document.body);
             return parent();
+        },
+
+        afterRender: function () {
+            this._refreshMessages();
+            $.registry.get(this.amountComponentConfig.name, document.body)?._updateAmount();
         }
     });
 })();
