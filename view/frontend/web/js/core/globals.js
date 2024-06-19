b82e34f01d4e485e586f140855877af6bb8c7140
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-06-19 10:41:40 +0300

    Move breezemap to define.js

--- view/frontend/web/js/core/globals.js
+++ view/frontend/web/js/core/globals.js
@@ -6,36 +6,4 @@
 
     $.breeze.loadedScripts = {};
     $.breeze.jsconfig = {};
-
-    $.breezemap = new Proxy({
-        'jquery': $,
-        'ko': ko,
-        'knockout': ko,
-        'underscore': _,
-        'mage/mage': $.mage,
-        __counter: 1,
-        __aliases: {},
-        __getAll: () => ({ ...$.breezemap }),
-        __get: key => $.breezemap[key],
-        __lastComponent: (offset = 0) => $.breezemap[`__component${$.breezemap.__counter - 1 - offset}`],
-        __register: (name, oldName) => {
-            if ($.breezemap[name]) {
-                return;
-            }
-
-            if (!oldName || _.isNumber(oldName)) {
-                $.breezemap[name] = $.breezemap.__lastComponent(oldName);
-            } else if ($.breezemap[oldName] !== undefined) {
-                $.breezemap[name] = $.breezemap[oldName];
-            }
-        }
-    }, {
-        set(obj, alias, value) {
-            obj[alias] = value;
-
-            $(document).trigger('breeze:component:load', { alias, value });
-
-            return true;
-        }
-    });
 })();
