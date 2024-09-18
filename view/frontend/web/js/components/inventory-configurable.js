c198917a010fbb1a6775f34290ebb955c9fbfae6
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-09-18 10:31:29 +0300

    Use $.mixinSuper utility

--- view/frontend/web/js/components/inventory-configurable.js
+++ view/frontend/web/js/components/inventory-configurable.js
@@ -4,13 +4,13 @@ define([
 ], function ($, configurableVariationQty) {
     'use strict';
 
-    $.mixin('configurable', {
-        _configureElement: function (o, element) {
+    $.mixinSuper('configurable', {
+        _configureElement: function (element) {
             var salesChannel = this.options.spConfig.channel,
                 salesChannelCode = this.options.spConfig.salesChannelCode,
                 productVariationsSku = this.options.spConfig.sku;
 
-            o(element);
+            this._super(element);
 
             configurableVariationQty(productVariationsSku[this.simpleProduct], salesChannel, salesChannelCode);
         }
