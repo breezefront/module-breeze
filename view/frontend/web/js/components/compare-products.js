eadb7bdacf16d87939d532c6656f262ca147cebd
Ibrahim Saleh <19901858+IbrahimS2@users.noreply.github.com>
2024-11-14 15:02:35 +0300

    Fixed not working "Print" button on compare page

--- view/frontend/web/js/components/compare-products.js
+++ view/frontend/web/js/components/compare-products.js
@@ -3,6 +3,18 @@
 
     $.view('compareProducts', {
         component: 'Magento_Catalog/js/view/compare-products',
-        compareProducts: $.customerData.get('compare-products')
+        compareProducts: $.customerData.get('compare-products'),
+    });
+
+    $.widget('mage.compareList', {
+        component: 'compareList',
+
+        /** @inheritdoc */
+        _create: function () {
+            $(this.options.windowPrintSelector).on('click', function (e) {
+                e.preventDefault();
+                window.print();
+            });
+        }
     });
 })();
