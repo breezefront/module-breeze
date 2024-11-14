b2c962b72d4fb0a394ee7c578b96f26d48847669
Ibrahim Saleh <19901858+IbrahimS2@users.noreply.github.com>
2024-11-14 13:47:38 +0300

    Update compare-products.js

--- view/frontend/web/js/components/compare-products.js
+++ view/frontend/web/js/components/compare-products.js
@@ -1,9 +1,9 @@
 (function () {
-    "use strict";
+    'use strict';
 
-    $.view("compareProducts", {
-        component: "Magento_Catalog/js/view/compare-products",
-        compareProducts: $.customerData.get("compare-products"),
+    $.view('compareProducts', {
+        component: 'Magento_Catalog/js/view/compare-products',
+        compareProducts: $.customerData.get('compare-products'),
     });
 
     $.widget('mage.compareList', {
@@ -11,33 +11,6 @@
 
         /** @inheritdoc */
         _create: function () {
-            var elem = this.element,
-                products = $('thead td', elem),
-                headings;
-
-            if (products.length > this.options.productsInRow) {
-                headings = $('<table></table>')
-                    .addClass('comparison headings data table')
-                    .insertBefore(elem.closest('.container'));
-
-                elem.addClass('scroll');
-
-                $('th', elem).each(function () {
-                    var th = $(this),
-                        thCopy = th.clone();
-
-                    th.animate({
-                        top: '+=0'
-                    }, 50, function () {
-                        var height = th.height();
-
-                        thCopy.css('height', height)
-                            .appendTo(headings)
-                            .wrap('<tr></tr>');
-                    });
-                });
-            }
-
             $(this.options.windowPrintSelector).on('click', function (e) {
                 e.preventDefault();
                 window.print();
