659140ff0f218fa8996c2cd10976e1dd9e3c3a4b
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-01-24 17:48:08 +0200

    Fixed invisible messages when first message was hidden automatically.

--- view/frontend/web/js/components/messages.js
+++ view/frontend/web/js/components/messages.js
@@ -125,7 +125,7 @@
                 if (e.animationName === 'message-hide') {
                     $(el).remove();
                     if (!messages.children().length) {
-                        messages.remove();
+                        messages.text('');
                     }
                 }
             });
