aa8cd27c7cf406723d5f15ecb9d536dc0f0e4bf3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-03-25 13:30:35 +0200

    Better fix for previous commit

--- view/frontend/web/js/components/line-clamp.js
+++ view/frontend/web/js/components/line-clamp.js
@@ -33,8 +33,7 @@
         },
 
         refresh() {
-            var isFullyVisible = this.element.hasClass('expanded') ||
-                this.element[0].scrollHeight === this.element[0].clientHeight;
+            var isFullyVisible = this.element[0].scrollHeight - this.element[0].clientHeight <= 5;
 
             // hide toggler if content is fully visible without expanding
             if (!this.element.hasClass('expanded') && isFullyVisible) {
