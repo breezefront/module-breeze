5b316a07608963f18c258b91949f884657b70ebd
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-10-07 10:33:08 +0300

    Allow use `$.cookies` and `$.breeze.getFormKey` in inline scripts

--- view/frontend/web/js/components/form-key.js
+++ view/frontend/web/js/components/form-key.js
@@ -7,36 +7,11 @@
         },
 
         _create: function () {
-            $(this.options.inputSelector).val($.cookies.get('form_key'));
+            $(this.options.inputSelector).val($.breeze.getFormKey());
         }
     });
 
-    function generateFormKeyString() {
-        var result = '',
-            length = 16,
-            chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        while (length--) {
-            result += chars[Math.round(Math.random() * (chars.length - 1))];
-        }
-
-        return result;
-    }
-
-    function initFormKey() {
-        var formKey = $.cookies.get('form_key');
-
-        if (!formKey) {
-            formKey = generateFormKeyString();
-            $.cookies.set('form_key', formKey, {
-                expires: 1
-            });
-        }
-
-        $('input[name="form_key"]').val(formKey);
-    }
-
     $(document).on('breeze:load contentUpdated', function () {
-        initFormKey();
+        $('input[name="form_key"]').val($.breeze.getFormKey());
     });
 })();
---
+++ view/frontend/web/js/core/form-key.js
@@ -0,0 +1,30 @@
+define([
+    'mage/cookies'
+], function (cookies) {
+    'use strict';
+
+    function generateFormKeyString() {
+        var result = '',
+            length = 16,
+            chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        while (length--) {
+            result += chars[Math.round(Math.random() * (chars.length - 1))];
+        }
+
+        return result;
+    }
+
+    $.breeze.getFormKey = function () {
+        var formKey = cookies.get('form_key');
+
+        if (!formKey) {
+            formKey = generateFormKeyString();
+            cookies.set('form_key', formKey, {
+                expires: 1
+            });
+        }
+
+        return formKey;
+    };
+});
