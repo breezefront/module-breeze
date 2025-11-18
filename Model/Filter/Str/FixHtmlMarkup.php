b73bb7baa30bffd66d096659049e15b2dfdb1d33
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-11-18 11:31:07 +0200

    Added fix for newer libxml: `data-mage-init="{".."}"`

--- Model/Filter/Str/FixHtmlMarkup.php
+++ Model/Filter/Str/FixHtmlMarkup.php
@@ -7,6 +7,7 @@ class FixHtmlMarkup
     public function process($html)
     {
         $html = $this->fixVarnishIncludes($html);
+        $html = $this->fixQuotesInDataMageInit($html);
         return $html;
     }
 
@@ -22,6 +23,32 @@ class FixHtmlMarkup
 
         foreach ($replaceMapping as $search => $replace) {
             $html = str_replace($search, $replace, $html);
+        }
+
+        return $html;
+    }
+
+    /**
+     * Newer libxml versions break markup of the widget rendered inside html element:
+     * data-mage-init='{".."}' becomes data-mage-init="{".."}"
+     * This code revert the broken data-mage-init attributes to data-mage-init='{".."}'
+     */
+    private function fixQuotesInDataMageInit($html)
+    {
+        $search = 'data-mage-init="{"';
+        $replace = 'data-mage-init=\'{"';
+        $searchLen = strlen($search);
+        $offset = 0;
+
+        while (($pos = strpos($html, $search, $offset)) !== false) {
+            $closePos = strpos($html, '}"', $pos + $searchLen);
+            if ($closePos === false) {
+                break;
+            }
+
+            $html = substr_replace($html, "}'", $closePos, 2);
+            $html = substr_replace($html, $replace, $pos, $searchLen);
+            $offset = $closePos + 2;
         }
 
         return $html;
